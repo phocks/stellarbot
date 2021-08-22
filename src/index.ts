94267668eb3f91a071216e53a52dce65ea31ed8a
Joshua Byrd <phocks@gmail.com>
2021-08-22 10:59:09 +1000

    Balance shown

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ require("dotenv").config();
 import { client } from "./client";
 import StellarSdk from "stellar-sdk";
 import to from "await-to-js";
+import fetch from "node-fetch";
+
+const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 
 const main = async () => {
   const [verifyError, verifyResult] = await to(
@@ -12,8 +15,17 @@ const main = async () => {
     return;
   }
 
-  const pair = StellarSdk.Keypair.random();
-  console.log(pair.publicKey());
+  // the JS SDK uses promises for most actions, such as retrieving an account
+  const account = await server.loadAccount(process.env.stellar_public);
+  console.log("Balances for account: " + process.env.stellar_public);
+  account.balances.forEach(function (balance: any) {
+    console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
+  });
+
+  // const pair = StellarSdk.Keypair.random();
+  // console.log(pair.publicKey());
+  // console.log(pair.secret());
+
   // const tweet = await client.post("statuses/update", {
   //   status: "Hello world!!",
   //   auto_populate_reply_metadata: true,
