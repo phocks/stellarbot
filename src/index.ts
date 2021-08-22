af669727d8ee63055bab5a0fa03a9619d9cd74fa
Joshua Byrd <phocks@gmail.com>
2021-08-23 08:00:00 +1000

    Address

--- src/index.ts
+++ src/index.ts
@@ -4,23 +4,41 @@ import StellarSdk from "stellar-sdk";
 import to from "await-to-js";
 import fetch from "node-fetch";
 
+const accountAddress = process.env.stellar_public;
+
 const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 
 const main = async () => {
-  const [verifyError, verifyResult] = await to(
-    client.get("account/verify_credentials")
-  );
-  if (verifyError) {
-    console.error(verifyError);
-    return;
-  }
+  // const [verifyError, verifyResult] = await to(
+  //   client.get("account/verify_credentials")
+  // );
+
+  // if (verifyError) {
+  //   console.error(verifyError);
+  //   return;
+  // }
 
   // the JS SDK uses promises for most actions, such as retrieving an account
-  const account = await server.loadAccount(process.env.stellar_public);
-  console.log("Balances for account: " + process.env.stellar_public);
+  const account = await server.loadAccount(accountAddress);
+  console.log("Balances for account: " + accountAddress);
   account.balances.forEach(function (balance: any) {
     console.log("Type:", balance.asset_type, ", Balance:", balance.balance);
   });
+
+  
+  var lastCursor = 0; // or load where you left off
+
+  var txHandler = function (txResponse: any) {
+    console.log(txResponse);
+  };
+
+  var es = server
+    .transactions()
+    .forAccount(accountAddress)
+    .cursor(lastCursor)
+    .stream({
+      onmessage: txHandler,
+    });
 
   // const pair = StellarSdk.Keypair.random();
   // console.log(pair.publicKey());
